da0b0d520806f418ae5ef49f633a6be674b411f4
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-06 21:57:06 +0530

    hormonic day 5

---
+++ hormonic day5.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information 
+using  system;
+class program
+{
+	public class Harmonic
+	{
+
+		public static void main(String[] args)
+		{
+
+			// INITIALIZATION
+			double result = 0, num = 0, j;
+			int n;
+			Utility u = new Utility();
+
+			// INPUT DATA
+			Console.WriteLine("enter the harmonic you want to find: ");
+			n = u.inputInteger();
+
+			// COMPUTATION
+			for (j = 1; j <= n; j++)
+			{
+				num = num + (1 / j);
+				result = num;
+			}
+			Console.WriteLine("the value of harmonic no. " + n + " is: " + result);
+
+		}
+
+	}
+}
