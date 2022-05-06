19c921676034a811b54c54ca1a0f81725446a98f
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-06 20:18:46 +0530

    day5 flip coin

---
+++ Flip coin day 5.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using system;
+namespace flipcoin
+{
+	class program
+	{
+		public class CoinToss
+		{
+			private enum Coin { Heads, Tails };
+			Random randomNum = new Random();
+			private int result;
+			//private int heads = 0;
+			//private int tails = 1;
+			Coin coinFlip;
+
+			public void flip()
+			{
+				result = randomNum.nextInt(2);
+
+				if (result == 0)
+				{
+					coinFlip = Coin.Heads;
+					Console.WriteLine("You flipped Heads!");
+				}
+				else
+				{
+					coinFlip = Coin.Tails;
+					public class CoinToss
+			{
+				private enum Coin { Heads, Tails };
+				Random randomNum = new Random();
+				private int result;
+				//private int heads = 0;
+				//private int tails = 1;
+				Coin coinFlip;
+
+				public void flip()
+				{
+					Random random = randomNum.nextInt(2);
+
+					if (result == 0)
+					{
+						coinFlip = Coin.Heads;
+						Console.WriteLine("You flipped Heads!");
+					}
+					else
+					{
+						coinFlip = Coin.Tails;
+					}
+				}
+			}
+		}
+	}
+}
