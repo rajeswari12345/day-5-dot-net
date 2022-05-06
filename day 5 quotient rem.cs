5d1226699aa60cf0f8ebb53af3f81e05aa8fdd95
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-06 20:18:03 +0530

    day 5 quotient

---
+++ day 5 quotient rem.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using system;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int dividend = 50, divisor = 8;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+        Console.WriteLine("Quotient = " + quotient);
+        Console.WriteLine("Remainder = " + remainder);
+        Console.ReadLine();
+    }
+}
